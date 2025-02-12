ae9f3fcd1d603b47806785372362b7f0b90e3fd2
Aaron Staes <70277183+aaronsts@users.noreply.github.com>
2025-02-12 22:15:02 +0100

    ci: semantic-release setup (#1)
    Fix #2

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+const commitlintConfig = {
+  extends: ["@commitlint/config-angular"],
+  parserPreset: "conventional-changelog-angular",
+  rules: {
+    "header-max-length": [2, "always", 72],
+    "body-max-line-length": [2, "always", 72],
+    "header-full-stop": [2, "never", "."],
+    "body-leading-blank": [2, "always"],
+  },
+};
+
+export default commitlintConfig;
