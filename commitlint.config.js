e6668b504120ad6eaa1dfa7a1be0365f9816ce52
Aaron Staes <70277183+aaronsts@users.noreply.github.com>
2025-04-20 07:54:46 +0200

    chore: update dependencies (#49)
    Update all dependecies to latest available version
    Fix #41

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,6 @@
 /* eslint-env node */
 const commitlintConfig = {
-  extends: ["@commitlint/config-angular"],
-  parserPreset: "conventional-changelog-angular",
-  rules: {
-    "header-max-length": [2, "always", 72],
-    "body-max-line-length": [2, "always", 72],
-    "header-full-stop": [2, "never", "."],
-    "body-leading-blank": [2, "always"],
-  },
+  extends: ["@commitlint/config-conventional"],
 };
 
 export default commitlintConfig;
